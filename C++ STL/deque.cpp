5bb9a87d1473c07981d22c2c1a70c967ae953042
kishanchauhan01 <kishanchauhan2006.25@gmail.com>
2025-01-03 22:12:18 +0530

    update

--- C++ STL/deque.cpp
+++ C++ STL/deque.cpp
@@ -1,45 +1,34 @@
-//Deque stands for Double Ended Queues. They are not allocated contiguous memory locations. Unlike queues, they allow both insertion and deletion at both ends. Although vector also allows us for insertion and deletion at both ends, these operations on deques are more efficient.
-//Deque is a data structure that inherits the properties of both queues and stacks. Additionally, the implementation of this data structure requires constant time, i.e., time complexity = O(1). This means you can use deque to your advantage to implement both the queue and stack.
+#include <bits/stdc++.h>
 #include <iostream>
-#include<deque>
 using namespace std;
 
 int main(){
-    
     deque<int> d;
-
+    
     d.push_back(1);
-    d.push_back(2);
-    d.push_back(3);
+    d.push_front(2);
 
     for(int i:d) {
         cout << i << " ";
     }cout << endl;
 
     // d.pop_back();
-
-    // for(int i:d) {
-    //     cout << i << " ";
-    // }cout << endl;
-
     // d.pop_front();
-
     // for(int i:d) {
     //     cout << i << " ";
     // }cout << endl;
 
-    cout << "Print first element --> " << d.at(1) << endl;
+    cout << "print first index element: " << d.at(1) << endl;
 
-    cout << "Front --> " << d.front() << endl;
-    cout << "back --> " << d.back() << endl;
+    cout << "front: " << d.front() << endl;
+    cout << "back: " << d.back() << endl;
 
-    cout << "Empty or not --> " << d.empty() << endl;
+    cout << "empty or not: " << d.empty()<< endl;
 
-    cout << "Before clear --> " << d.size() << endl;
-    d.erase(d.begin(), d.begin()+1);
-    cout << "After clear --> " << d.size() << endl;
+    cout << "Before erase: " << d.size() << endl;
+    d.erase(d.begin(), d.end());
+    cout << "Before erase: " << d.size() << endl;
 
-    for(int i:d) {
-        cout << i << " ";
-    }cout << endl;
+
+    return 0;
 }
---
+++ C++ STL/vector.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    vector<int> a(5,1);//here 5 is size of arr and initalized with 1 by default 0
+    vector<int> last(a)//copy the a vector into last vector
+
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    v.push_back(1);
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    v.push_back(2);
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    v.push_back(3);
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    v.push_back(4);
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    v.push_back(5);
+    cout << "Capacity of v:- " << v.capacity() << endl;
+    //capacity is increase by 2 means in multiple of two
+
+    cout << "size of v:- " << v.size() << endl;
+
+    cout << "element at 2 index:- " << v.at(2) << endl;
+
+    cout << "front: " << v.front() << endl;
+    cout << "back: " << v.back() << endl;
+
+    cout << "before pop" << endl;
+    for(int i:v) {
+        cout << i << " ";
+    }cout << endl;
+
+    v.pop_back();
+
+    cout << "after pop" << endl;
+    for(int i:v) {
+        cout << i << " ";
+    }cout << endl;
+
+    cout << "before clearing the vector: " << v.size() << endl;
+    v.clear();
+    cout << "after clearing the vector: " << v.size() << endl;
+
+    return 0;
+}
